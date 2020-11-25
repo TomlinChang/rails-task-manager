aafa19d1cda2bed040340b66f5b1e5954b806062
tom chang <tomlin83@gmail.com>
2020-11-25 09:12:18 +0800

    finished exercise but fully complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  get 'list', to: 'tasks#list'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  get 'tasks/:id', to: 'tasks#find', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task
 end
